875b99b4e267713ee6567838aed007418d37f42d
Edgar G Vargas Ocampo <egiovanni.vo@gmail.com>
2024-07-14 19:58:05 -0600

    db and migrations added

--- app/database/initDB.go
+++ app/database/initDB.go
@@ -3,6 +3,8 @@ package database
 import (
 	"context"
 	"log"
+	"os"
+	"os/exec"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -17,6 +19,25 @@ func InitDB(ctx context.Context) {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
 
+	runMigrations()
+
 	Q = New(Conn)
 	log.Println("Connected to the database")
+}
+
+func runMigrations() {
+	envVars := []string{
+		"DB_URL=" + os.Getenv("DB_URL"),
+		"MIGRATIONS_PATH=" + os.Getenv("MIGRATIONS_PATH"),
+		"SCHEMA_PATH=" + os.Getenv("SCHEMA_PATH"),
+		"DB_DEV_URL=" + os.Getenv("DB_DEV_URL"),
+	}
+
+	applyCmd := exec.Command("atlas", "migrate", "apply", "--url", os.Getenv("DB_URL"))
+	applyCmd.Env = append(os.Environ(), envVars...)
+	if output, err := applyCmd.CombinedOutput(); err != nil {
+		log.Fatalf("Error aplicando migraciones: %s\n%s", err, output)
+	}
+
+	log.Println("Migraciones aplicadas correctamente.")
 }
