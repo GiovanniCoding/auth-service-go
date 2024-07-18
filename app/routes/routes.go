88f3e68f095c7738cc10ae97f37114932ce20f84
Edgar G Vargas Ocampo <egiovanni.vo@gmail.com>
2024-07-17 22:58:20 -0600

    swagger info

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -9,7 +9,12 @@ import (
 )
 
 func SetupRoutes(router *gin.Engine) {
+	docs.SwaggerInfo.Version = "0.1.0"
+	docs.SwaggerInfo.Host = "localhost:8080"
 	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.Schemes = []string{"http"}
+	docs.SwaggerInfo.Title = "Microservice Auth"
+	docs.SwaggerInfo.Description = "Microservice Auth"
 
 	api := router.Group("/api/v1")
 	api.POST("/register", handlers.Register)
