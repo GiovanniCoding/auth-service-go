09a9ea2905c761f3e7d7cf932b434c7ffb86304f
Edgar G Vargas Ocampo <egiovanni.vo@gmail.com>
2024-07-24 22:27:55 -0600

    request body added

--- app/middlewares/log.go
+++ app/middlewares/log.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"bytes"
+	"io"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -17,9 +19,17 @@ func LogMiddleware() gin.HandlerFunc {
 		ctx.Set("traceID", traceID)
 		Logger = log.With().Str("trace_id", traceID).Logger()
 
+		var requestBody []byte
+		if ctx.Request.Body != nil {
+			requestBody, _ = io.ReadAll(ctx.Request.Body)
+			ctx.Request.Body = io.NopCloser(bytes.NewBuffer(requestBody))
+		}
+
 		Logger.Info().
 			Str("url", ctx.Request.RequestURI).
 			Str("method", ctx.Request.Method).
+			Str("client_ip", ctx.ClientIP()).
+			Bytes("request_body", requestBody).
 			Msg("request")
 
 		start := time.Now()
@@ -31,6 +41,7 @@ func LogMiddleware() gin.HandlerFunc {
 		Logger.Info().
 			Int("status", ctx.Writer.Status()).
 			Dur("latency", stop.Sub(start)).
+			Int("response_size", ctx.Writer.Size()).
 			Msg("response")
 	}
 }
