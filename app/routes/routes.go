2a334b56b97b14c629ac929ee819b6731df6670a
Edgar G Vargas Ocampo <egiovanni.vo@gmail.com>
2024-09-07 22:33:42 -0600

    health endpoint added

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -8,6 +8,8 @@ import (
 func SetupRoutes(router *gin.Engine) {
 	api := router.Group("/api/v1")
 
+	api.GET("/health", handlers.HealthCheck)
+
 	api.POST("/login", handlers.Login)
 	api.POST("/register", handlers.Register)
 
